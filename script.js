59fd926e7f501f0112bd674cb35e91ed5bbd25b6
kpaulemon <kwpaulemon23@gmail.com>
2025-09-27 13:29:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+ document.addEventListener('DOMContentLoaded', () => {
+        const heading = document.querySelector('h1');
+        heading.addEventListener('click', () => {
+            heading.textContent = 'Clicked!';
+        });
+    });
