83936aa6a859f456547a5f163392099d15cdd339
kpaulemon <kwpaulemon23@gmail.com>
2025-09-28 10:26:01 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
- document.addEventListener('DOMContentLoaded', () => {
-        const heading = document.querySelector('h1');
-        heading.addEventListener('click', () => {
-            heading.textContent = 'Clicked!';
-        });
-    });
